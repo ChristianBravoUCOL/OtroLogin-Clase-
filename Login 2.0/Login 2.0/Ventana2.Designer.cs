35fb634cf3184d575d2f3414d401e786455e10b0
Docente <Docente@PCMAESTRO>
2019-08-26 09:56:16 -0500

    Ya corre mas cosas

---
+++ Login 2.0/Login 2.0/Ventana2.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Login_2._0
+{
+    partial class Ventana2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbC = new System.Windows.Forms.CheckBox();
+            this.cbB = new System.Windows.Forms.CheckBox();
+            this.rbTarjeta = new System.Windows.Forms.RadioButton();
+            this.rbPay = new System.Windows.Forms.RadioButton();
+            this.btnComprar = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cursos en Venta";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(373, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 34);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Formas de Pago";
+            // 
+            // cbC
+            // 
+            this.cbC.AutoSize = true;
+            this.cbC.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbC.Location = new System.Drawing.Point(18, 131);
+            this.cbC.Name = "cbC";
+            this.cbC.Size = new System.Drawing.Size(123, 29);
+            this.cbC.TabIndex = 2;
+            this.cbC.Text = "Visual C#";
+            this.cbC.UseVisualStyleBackColor = true;
+            // 
+            // cbB
+            // 
+            this.cbB.AutoSize = true;
+            this.cbB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbB.Location = new System.Drawing.Point(18, 202);
+            this.cbB.Name = "cbB";
+            this.cbB.Size = new System.Drawing.Size(149, 29);
+            this.cbB.TabIndex = 3;
+            this.cbB.Text = "Visual Basic";
+            this.cbB.UseVisualStyleBackColor = true;
+            // 
+            // rbTarjeta
+            // 
+            this.rbTarjeta.AutoSize = true;
+            this.rbTarjeta.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbTarjeta.Location = new System.Drawing.Point(377, 130);
+            this.rbTarjeta.Name = "rbTarjeta";
+            this.rbTarjeta.Size = new System.Drawing.Size(202, 29);
+            this.rbTarjeta.TabIndex = 4;
+            this.rbTarjeta.TabStop = true;
+            this.rbTarjeta.Text = "Tarjeta de Crédito";
+            this.rbTarjeta.UseVisualStyleBackColor = true;
+            // 
+            // rbPay
+            // 
+            this.rbPay.AutoSize = true;
+            this.rbPay.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbPay.Location = new System.Drawing.Point(377, 201);
+            this.rbPay.Name = "rbPay";
+            this.rbPay.Size = new System.Drawing.Size(98, 29);
+            this.rbPay.TabIndex = 5;
+            this.rbPay.TabStop = true;
+            this.rbPay.Text = "PayPal";
+            this.rbPay.UseVisualStyleBackColor = true;
+            // 
+            // btnComprar
+            // 
+            this.btnComprar.Location = new System.Drawing.Point(18, 282);
+            this.btnComprar.Name = "btnComprar";
+            this.btnComprar.Size = new System.Drawing.Size(133, 79);
+            this.btnComprar.TabIndex = 6;
+            this.btnComprar.Text = "&Comprar";
+            this.btnComprar.UseVisualStyleBackColor = true;
+            this.btnComprar.Click += new System.EventHandler(this.BtnComprar_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(446, 282);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(133, 79);
+            this.btnCerrar.TabIndex = 7;
+            this.btnCerrar.Text = "&Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.BtnCerrar_Click);
+            // 
+            // Ventana2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 387);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnComprar);
+            this.Controls.Add(this.rbPay);
+            this.Controls.Add(this.rbTarjeta);
+            this.Controls.Add(this.cbB);
+            this.Controls.Add(this.cbC);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Ventana2";
+            this.Text = "Cursos de Programación";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox cbC;
+        private System.Windows.Forms.CheckBox cbB;
+        private System.Windows.Forms.RadioButton rbTarjeta;
+        private System.Windows.Forms.RadioButton rbPay;
+        private System.Windows.Forms.Button btnComprar;
+        private System.Windows.Forms.Button btnCerrar;
+    }
+}
---
+++ Login 2.0/Login 2.0/Ventana2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_2._0
+{
+    public partial class Ventana2 : Form
+    {
+        public Ventana2()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 formulario = new Form1();
+            formulario.Visible = true;
+        }
+
+        private void BtnComprar_Click(object sender, EventArgs e)
+        {
+            int conteo = 0;
+            string seleccion;
+            if(cbC.Checked == true)
+            {
+                conteo++;
+            }
+            if (cbB.Checked == true)
+            {
+                conteo++;
+            }
+            if(rbTarjeta.Checked==true)
+            {
+                seleccion = "Tarjeta de Crédito";
+            }
+            else
+            {
+                seleccion = "PayPal";
+            }
+        }
+    }
+}
